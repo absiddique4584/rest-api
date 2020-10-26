c3335b5c4539bfa62538f40d87aee1d783ff88ca
mdsumon7914 <mdsumon7914@gmail.com>
2020-10-26 20:38:10 +0600

    rest-api store method done by postman

--- app/Http/Controllers/Api/SclassController.php
+++ app/Http/Controllers/Api/SclassController.php
@@ -36,7 +36,16 @@ class SclassController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData =$request->validate([
+           'class_name' =>  'required|unique:sclasses|max:25'
+        ]);
+
+
+        $data = array();
+        $data['class_name'] = $request->class_name;
+        $insert = DB::table('sclasses')->insert($data);
+        return response('Data Inserted Successfully ! ');
+
     }
 
     /**
