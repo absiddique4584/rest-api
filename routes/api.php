5d5fe5fb7fab71921908c39636e87951d204496c
mdsumon7914 <mdsumon7914@gmail.com>
2020-11-02 16:25:39 +0600

    integrate jwt authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,25 @@
 <?php
 
 
+
+
+Route::group([
+
+    //'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('register', 'AuthController@register');
+
+});
+
+
+
 Route::ApiResource('/class','Api\SclassController');
 Route::ApiResource('/subject','Api\SubjectController');
 Route::ApiResource('/section','Api\SectionController');
